eae55de61127ef35f69f12e52c2cb3919439c312
Richard5A <richard5a.dev@gmail.com>
2025-05-03 13:18:17 +0200

    Creating login component

--- calendarwebsite/src/app/app.routes.ts
+++ calendarwebsite/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "login",
+    loadComponent: async () => {
+      const m = await import('./login/login.component');
+      return m.LoginComponent;
+    },
+  }
+];
