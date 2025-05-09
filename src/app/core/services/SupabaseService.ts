97d7593b7b7af6b8d946d2328aa7b2a8316905b9
Richard5A <richard5a.dev@gmail.com>
2025-05-09 15:32:52 +0200

    Add SupabaseService.ts and login component

---
+++ src/app/core/services/SupabaseService.ts
@@ -0,0 +1,55 @@
+import {Injectable, NgZone} from '@angular/core';
+import {
+  AuthChangeEvent, AuthError,
+  createClient,
+  Session,
+  SignInWithPasswordCredentials,
+  SupabaseClient,
+  User
+} from '@supabase/supabase-js';
+import {BehaviorSubject} from 'rxjs';
+import {Router} from '@angular/router';
+import {environment} from '../../../environments/environment';
+
+@Injectable({
+  providedIn: "root"
+})
+export class SupabaseService {
+  private supabase: SupabaseClient
+  private currentUserSubject = new BehaviorSubject<User | null>(null);
+
+  constructor(private router: Router, private ngZone: NgZone) {
+    this.supabase = createClient(environment.supabaseUrl, environment.supabaseAnonKey);
+
+    this.supabase.auth.getSession().then(({ data }) => {
+      this.currentUserSubject.next(data.session?.user ?? null);
+    });
+
+    // Listen to auth changes
+    this.supabase.auth.onAuthStateChange((event: AuthChangeEvent, session: Session | null) => {
+      // Run inside Angular zone to ensure UI updates correctly
+      this.ngZone.run(() => {
+        const user = session?.user ?? null;
+        this.currentUserSubject.next(user);
+        console.log(`Supabase auth state changed: ${event}`, session);
+
+        // if (event === 'SIGNED_OUT') this.router.navigate(['/login']);
+      });
+    });
+  }
+
+  get supabaseClient(): SupabaseClient {
+    return this.supabase;
+  }
+
+  async signInWithEmail(credentials: SignInWithPasswordCredentials): Promise<AuthError | null> {
+    console.log("Logging in with email:", credentials);
+    const { error } = await this.supabase.auth.signInWithPassword(credentials);
+    return error;
+  }
+
+  async signOut(): Promise<AuthError | null> {
+    const { error } = await this.supabase.auth.signOut();
+    return error;
+  }
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
+import {MatError, MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {merge} from 'rxjs';
+import {SupabaseService} from '../core/services/SupabaseService';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    MatFormField,
+    MatInput,
+    ReactiveFormsModule,
+    MatButton,
+    MatLabel,
+    MatError
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.less'
 })
 export class LoginComponent {
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
+  readonly password = new FormControl('', [Validators.required])
+  errorMessage = signal("");
+  private supabase = inject(SupabaseService)
 
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateEmailErrorMessage());
+  }
+
+  updateEmailErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.errorMessage.set('You must enter a value');
+    } else if (this.email.hasError('email')) {
+      this.errorMessage.set('Not a valid email');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
+
+  login() {
+    const email = this.email.value;
+    const password = this.password.value;
+    if (email == null || email == "" || password == null || password == "") {
+      this.errorMessage.set('Please fill in all fields');
+      return;
+    }
+    if (this.email.hasError('required') || this.email.hasError('email')) {
+      this.errorMessage.set('Please enter a valid email');
+      return;
+    }
+    this.supabase.signInWithEmail({
+      email: email,
+      password: password,
+    })
+  }
 }
