eb5a837e5a835ccb9dfc707ace393b7a185d27fe
Richard5A <richard5a.dev@gmail.com>
2025-05-09 15:11:28 +0200

    Add Supabase configuration to environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+  supabaseUrl: "https://vhsxebsoxhjtqnrmahmc.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoc3hlYnNveGhqdHFucm1haG1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI1NzAxMzEsImV4cCI6MjA1ODE0NjEzMX0.2XE-KYJvE2FXISVs8QdHuo3OWCgL8RJNNqxRR95ArYY"
+};
